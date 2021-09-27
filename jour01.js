fb951bf1095b43d34ff2615e930edbb8f40bf179
Ahmed24 <ahmed24@MacBook-Pro-de-Ahmed24.local>
2021-09-27 13:27:10 +0200

    first  commit

---
+++ jour01.js
@@ -0,0 +1,26 @@
+// console.log("Helle Word")
+// var test ="Ahmed"
+// console.log("test")
+ 
+// var firstName = "xxx"
+// firstName = "elmutsim"
+// console.log("elmutsim + ahmed")
+// var testLength = "I'm very long !"
+//  console.log(testLength)
+// var food = "croissant is meh"
+// food = "so good"
+// console.log(food)
+var basic = "This is cool"
+var basicUP = "BASIC"
+var basicDown = "basic"
+console.log(basic)
+// var word = "banana"
+// var letters = word.split()
+// console.log(letters)
+var age = 30
+var  template = `im ${age} years old`
+console.log(template)
+ 
+
+
+
