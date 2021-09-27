dc450ba786a62472d52954bbb6a7feb6ba3b69e0
ahmed00E <84589474+ahmed00E@users.noreply.github.com>
2021-09-27 23:41:41 +0200

    Update jour01.js

--- jour01.js
+++ jour01.js
@@ -1,25 +1,30 @@
 // console.log("Helle Word")
-// var test ="Ahmed"
-// console.log("test")
+// var test ="My name is  Ahmed"
+// console.log(test)
  
-// var firstName = "xxx"
-// firstName = "elmutsim"
-// console.log("elmutsim + ahmed")
-// var testLength = "I'm very long !"
-//  console.log(testLength)
-// var food = "croissant is meh"
-// food = "so good"
-// console.log(food)
+var name = "Ahmed"
+//  console.log(`Nice to meet you" + " " ${name})
+ 
+var testLength = "I'm very long !"
+//  console.log(testLength.length)
+
+var food = "croissant is meh"
+food = "so good"
+console.log(food)
+
 var basic = "This is cool"
-var basicUP = "BASIC"
-var basicDown = "basic"
-console.log(basic)
-// var word = "banana"
-// var letters = word.split()
-// console.log(letters)
+var basicUP = basic.toUpperCase()
+var basicDown = basic.toLowerCase()
+// console.log(basic , basicUP , basicDown )
+
+var word = "banana"
+var letters = word.split("")
+console.log(letters)
+
+
 var age = 30
 var  template = `im ${age} years old`
-console.log(template)
+// console.log(template)
  
 
 
